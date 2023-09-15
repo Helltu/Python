99c7d23863d8ad52c4dd5351e2db4cd69a4c2ba3
Hellty <korsovm@gmail.com>
2023-09-15 14:53:02 +0300

    fix: improve set task methods

--- Task6.py
+++ Task6.py
@@ -1,9 +1,9 @@
 import random
 
-set1 = {random.randint(-100, 100) for i in range(20)}
-set2 = {random.randint(-100, 100) for i in range(20)}
+set1 = {random.randint(-10, 10) for i in range(20)}
+set2 = {random.randint(-10, 10) for i in range(20)}
 print('Полученные множества: ')
 print(set1)
 print(set2)
 print('Элементы первого множества, которые встречаются во втором множестве: ')
-print(set1.difference(set1.difference(set2)))
+print(set1 & set2)
