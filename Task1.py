27f9364cb13b4316b42ccff0689dd881fe2b02f4
Hellty <korsovm@gmail.com>
2023-09-15 16:35:28 +0300

    fix: better output

--- Task1.py
+++ Task1.py
@@ -3,6 +3,6 @@ if number.isdigit():
     SUMM = 0
     for i in range(0, len(number), 2):
         SUMM += int(number[i])
-    print(SUMM)
+    print('Сумма четных цифр введенного натурального числа: ' + str(SUMM))
 else:
     print('Вы ввели не натуральное число')
