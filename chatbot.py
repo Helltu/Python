ba5abe3e459128d52520486cc755d388b5ffad1d
Hellty <korsovm@gmail.com>
2023-11-26 17:47:49 +0300

    feat: done task

---
+++ chatbot.py
@@ -0,0 +1,57 @@
+import g4f
+import asyncio
+from aiogram import Dispatcher, Bot, types
+
+token = '6704974378:AAGmoZPMpLLFVqPEPfWQnK8HN2BPE-32Obc'
+g4f.debug.logging = True
+g4f.check_version = False
+
+message_storage = []
+
+
+def split_string(input_string, chunk_size):
+    return [input_string[i:i+chunk_size] for i in range(0, len(input_string), chunk_size)]
+
+
+async def start_bot(bot: Bot):
+    await bot.send_message(1392464735, text='Бот запущен!')
+
+
+async def stop_bot(bot: Bot):
+    await bot.send_message(1392464735, text='Бот завершил свою работу!')
+
+
+async def gpt(message: types.Message):
+    await message.bot.send_chat_action(message.chat.id, 'typing')
+    try:
+        message_storage.append({'role': 'user', 'content': message.text})
+        response = await g4f.ChatCompletion.create_async(
+            model=g4f.models.gpt_35_turbo,
+            provider=g4f.Provider.You,
+            messages=message_storage
+        )
+        message_storage.append({'role': 'assistant', 'content': response})
+        for item in split_string(response, 4096):
+            await message.answer(item)
+    except Exception as e:
+        await message.answer(f'Я не могу ответить на этот вопрос, по причине: {e}')
+
+
+async def start():
+    bot = Bot(token)
+
+    dp = Dispatcher()
+
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
+
+    dp.message.register(gpt)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        stop_bot(bot)
+        await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(start())
